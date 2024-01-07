c07d3df079a8bea0fb7390566598e0e48abadce7
jradnan <mohd.yasin085208@gmail.com>
2024-01-07 20:12:29 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import "./App.css";
 import Banner from "./Components/Banner/Banner";
 import RecomendedFood from "./Components/Foodapi/RecomendedFood";
+import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import FoodApi from "./Components/foodapi/FoodApi";
 
 function App() {
   return (
-    <div className="main-section">
-      <Navbar></Navbar>
-      <Banner></Banner>
-      <FoodApi></FoodApi>
-      <RecomendedFood></RecomendedFood>
-      
-    </div>
+    <>
+      <div className="main-section">
+        <Navbar></Navbar>
+        <Banner></Banner>
+        <FoodApi></FoodApi>
+        <RecomendedFood></RecomendedFood>
+      </div>
+      <Footer></Footer>
+    </>
   );
 }
 
